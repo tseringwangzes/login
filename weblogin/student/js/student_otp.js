36a6033c15886ba5e5cf22f129b8505c1a3498dc
tseringwangzes <92942144+tseringwangzes@users.noreply.github.com>
2023-01-19 01:26:38 +0530

    Add files via upload

---
+++ weblogin/student/js/student_otp.js
@@ -0,0 +1,19 @@
+const otp=document.querySelectorAll('.otp_field');
+//focus on first input
+otp[0].focus();
+
+otp.forEach((field, index)=>{
+    field.addEventListner('keydown', (e) =>{
+        if(e.key>=0 && e.key<=9){
+            otp[index].value ="";
+            setTimeout(()=>{
+                otp[index+1].focus();
+            },4);
+        }
+        else if(e.key == 'Backspace'){
+            setTimeout(()=>{
+                otp[index-1].focus();
+            },4);  
+        }
+    });
+});
